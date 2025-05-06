1ece25f48d30c32f0ad409fc86b9e6a0e9f17226
gihyeon <gihyeon.lee@gmail.com>
2025-05-06 13:38:45 +0900

    Update Ch3/L2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     get_num_chars_dict,
@@ -23,7 +24,10 @@ def print_report(book_path: str, num_words: int, num_chars_list: list) -> None:
 
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars_dict = get_num_chars_dict(text)
